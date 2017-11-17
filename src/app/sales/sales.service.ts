af7ee1895f3c993fde725e40aa86fd017b640b31
Kevin Boedigheimer <kevin.boedigheimer@sportngin.com>
2017-11-17 12:54:39 -0600

    sale service list sales

--- src/app/sales/sales.service.ts
+++ src/app/sales/sales.service.ts
@@ -1,8 +1,45 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {of} from 'rxjs/observable/of';
+import {Sale} from './sale';
+import {environment} from '../../environments/environment';
+import {catchError, map, tap} from 'rxjs/operators';
+import {ApiEnvelope} from '../api-envelope';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json', 'format': 'json' })
+};
 
 @Injectable()
 export class SalesService {
 
-  constructor() { }
 
+  constructor(private http: HttpClient) {
+  }
+
+  getSales(): Observable<ApiEnvelope<Sale[]>> {
+    return this.http.get<ApiEnvelope<Sale[]>>(this.salesUrl()).pipe(
+      catchError(this.handleError('getSales', new ApiEnvelope<Sale[]>())));
+  }
+
+  getSale(id): Observable<Sale> {
+    return this.http.get<Sale>(this.saleUrl(id))
+  }
+
+
+  private salesUrl(): string {
+    return environment.apiBase + '/sales.json';
+  }
+
+  private saleUrl(id): string {
+    return this.salesUrl() + '/' + id
+  }
+
+  private handleError<T>(operation: string, result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error)
+      return of(result as T);
+    }
+  }
 }
