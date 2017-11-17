f53ed67b986ca7e71a3d665ef8884850410e957f
Jake Merringer <jake.merringer@sportngin.com>
2017-11-17 11:43:32 -0600

    sidenav layout and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {MatToolbarModule} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule, MatSidenavModule, MatListModule} from '@angular/material';
+
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './/app-routing.module';
 import {SalesComponent} from './sales/sales.component';
@@ -16,8 +18,11 @@ import {DashboardComponent} from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
